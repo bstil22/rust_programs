20f0cd69c17a153b39d7e813a2498c61d2bf9415
bstil22 <brady.stilwell@csaa.com>
2016-03-05 22:08:00 -0700

    working

--- fib/src/lib.rs
+++ fib/src/lib.rs
@@ -1,3 +1,17 @@
-#[test]
-fn it_works() {
+use std::fmt::{self, Formatter, Display};
+fn main() {
+    let values: Vec<i32> = (1..42).map(|x| fib(x)).collect();
+    for x in &values {
+      print!("{} ", x);
+    }
+}
+
+fn fib(x: i32) -> i32 {
+    if x == 0 {
+        return 0;
+    } else if x == 1 {
+        return 1;
+    } else {
+        return fib(x-1) + fib(x-2);
+    }
 }
